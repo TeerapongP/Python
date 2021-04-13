65de9004be6c49878863a1504433c7621b3e2d06
TeerapongP <56886497+TeerapongP@users.noreply.github.com>
2021-04-13 22:17:15 +0700

    vscode://vscode.github-authentication/did-authenticate?windowid=1&code=51203e25ec60d7308282&state=970f1fca-bb63-4b97-9cb6-c859ad51805b

---
+++ Exercise 4.py
@@ -0,0 +1,12 @@
+Foundation_English = str(74)
+General_Business = str(70)
+Introduction_to_Computer_Systems = str(80)
+Computer_Programming = str(90)
+
+print("Foundation English : "+Foundation_English)
+print("General Business : "+ General_Business)
+print("Introduction to Computer Systems : "+ Introduction_to_Computer_Systems)
+print("Computer Programming : " + Computer_Programming)
+
+
+
---
+++ Lecture24.py
@@ -0,0 +1,9 @@
+# pi = 3.14
+# x = 24
+# print (x)
+# y = 5
+# print (y+x)
+name = "Teerapong"
+last_name = "Pinkaew"
+height = 1.68
+weight = 74.5
