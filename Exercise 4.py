c72eb1be13436ff06903abfe13d344c71bbd36ff
TeerapongP <56886497+TeerapongP@users.noreply.github.com>
2021-04-13 22:18:51 +0700

    Edit Score

--- Exercise 4.py
+++ Exercise 4.py
@@ -1,7 +1,7 @@
-Foundation_English = str(74)
-General_Business = str(70)
-Introduction_to_Computer_Systems = str(80)
-Computer_Programming = str(90)
+Foundation_English = str(84)
+General_Business = str(80)
+Introduction_to_Computer_Systems = str(90)
+Computer_Programming = str(100)
 
 print("Foundation English : "+Foundation_English)
 print("General Business : "+ General_Business)
