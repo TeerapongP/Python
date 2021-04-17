eb77fd6c596730c05573e93f6b3d493982563c7e
TeerapongP <56886497+TeerapongP@users.noreply.github.com>
2021-04-17 15:46:40 +0700

    add

---
+++ Lecture 54.py
@@ -0,0 +1,38 @@
+
+def login():
+    username = input("ENTER USERNAME :")
+    passw = input("ENTER PASSWORD :")
+    if username == "admin" and passw == "1234":
+        return True
+    else:
+        return False
+
+
+def ShowMenu():
+    print("------MangaBook------")
+    print("1. vat Calculate")
+    print("2. Price Calculate")
+
+
+def MenuSelect():
+    userSelected = int(input(">>"))
+    return userSelected
+
+
+def vatCalculate(priceCalculate):
+    vat = 7 / 100
+    result = priceCalculate + (priceCalculate * vat)
+    return result
+
+
+def priceCalculate():
+    price1 = int(input("First Product Price : "))
+    price2 = int(input("Second Product Price : "))
+    return vatCalculate(price1 + price2)
+
+login()
+ShowMenu()
+MenuSelect()
+print(vatCalculate(priceCalculate=int(input("ENTER PRICE : "))))
+print(priceCalculate(),"THB")
+
---
+++ Lecture 55.py
@@ -0,0 +1,6 @@
+import math
+
+print(math.ceil(10.49))
+print(math.floor(10.49))
+print(math.factorial(4))
+print(math.pow(9.5,2))
