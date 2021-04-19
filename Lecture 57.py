bd63ba3246f9b331e23059d32815445679b3a072
TeerapongP <56886497+TeerapongP@users.noreply.github.com>
2021-04-19 16:59:35 +0700

    add

---
+++ Lecture 57.py
@@ -0,0 +1,2 @@
+adress = "4/57 BangBon BangKok"
+print("BangKok"in adress)
---
+++ Lecture 59.py
@@ -0,0 +1,9 @@
+# name = input("ENTER NAME : ")
+# # lastName = input("ENTER LAST NAME : ")
+# #
+# # print("HELLO !! %s  %s " %(name.capitalize(),lastName.capitalize()))
+#
+# text = "POP"
+# textFormatted = "Wellcome %s"%(name.capitalize())
+# print(textFormatted.center(22,"-"))
+# print(len(name))
