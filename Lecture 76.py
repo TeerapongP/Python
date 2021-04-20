386693955cf7763f0c085da57f7782a8a74c062b
TeerapongP <56886497+TeerapongP@users.noreply.github.com>
2021-04-20 15:59:26 +0700

    add

--- Lecture 76.py
+++ Lecture 76.py
@@ -1,3 +1,8 @@
-Member_Bnk = {"Noey","Pape","Namneung"}
-Member_Bnk.remove("Pape")
-print(Member_Bnk)
+User_Input = int(input("Enter Number of your favorite fruits :"))
+
+My_Friust = set()
+
+while(len(My_Friust)<User_Input):
+
+    My_Friust.add(input("Friust : "))
+    print(My_Friust)
