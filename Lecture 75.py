4b4ee6e3043413957b191dd95b9a4b53baf501fe
TeerapongP <56886497+TeerapongP@users.noreply.github.com>
2021-04-20 15:50:08 +0700

    add'

---
+++ Lecture 75.py
@@ -0,0 +1,7 @@
+Member_Bnk = {"Noey","Pape","Namneung"}
+
+for i in Member_Bnk:
+    print(i)
+
+Member_Bnk.add("Mobile")
+print(Member_Bnk)
---
+++ Lecture 76.py
@@ -0,0 +1,3 @@
+Member_Bnk = {"Noey","Pape","Namneung"}
+Member_Bnk.remove("Pape")
+print(Member_Bnk)
